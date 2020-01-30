a3d9977e114194085ca5fff5ebe856ff1f8d0f94
naderalfakesh <nader.alfakesh@gmail.com>
2020-01-30 19:41:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2; 
+let num2 = 62/2;
+let multiply = num1 *num2 ; 
+
+let random = Math.floor( Math.random()*10 )+1;
+
+let mod = (22%4)*2;
+
+let max = Math.max(1,2,3,4,5,6,7,20);
